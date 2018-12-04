e0feac256df2f747909cc51d3205e6be22cd18b9
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-12-04 17:02:13 +0100

    fixed source script issue - did not execute the first boot commands

--- scripts/mgmt/install-soundcard.sh
+++ scripts/mgmt/install-soundcard.sh
@@ -5,19 +5,20 @@ log "Installing soundcard ${target_sndcard}"
 case $target_sndcard in
     "audioinjector")
         echo "> execute \"soundcards/install-audioinjector.sh\""
-        ./soundcards/install-audioinjector.sh
+        # must be sourced because relies on _globals.sh
+        source ./soundcards/install-audioinjector.sh
         ;;
     "hifiberry")
         echo "> execute \"soundcards/install-hifiberry.sh\""
-        ./soundcards/install-hifiberry.sh
+        source ./soundcards/install-hifiberry.sh
         ;;
     "raspiaudio")
         echo "> execute \"soundcards/install-raspiaudio.sh\""
-        ./soundcards/install-raspiaudio.sh
+        source ./soundcards/install-raspiaudio.sh
         ;;
     "usb")
         echo "> execute \"soundcards/install-usb-audio.sh\""
-        ./soundcards/install-usb-audio.sh
+        source ./soundcards/install-usb-audio.sh
         ;;
     "none")
         echo "> installing no soundcard"
